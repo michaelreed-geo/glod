729deaefbfe8e79cfbdfc62ae7e1ef93232afd21
michaelreed-geo <michael.reed5@outlook.com>
2026-02-15 19:34:08 +0000

    feat/ initial commit of FeatureCollection

--- glod/__init__.py
+++ glod/__init__.py
@@ -1,5 +1,5 @@
 from .config import set_use_pyproj
-from .feature import Feature
+from .feature import Feature, FeatureCollection
 from .geometry import Geometry
 
-__all__ = ["Feature", "Geometry", "set_use_pyproj"]
+__all__ = ["Feature", "FeatureCollection", "Geometry", "set_use_pyproj"]
--- glod/feature.py
+++ glod/feature.py
@@ -1,11 +1,23 @@
 from .geometry import Geometry
 from .utils import CRSType, feature_to_geojson
 
+import os
+import json
+
 
 class Feature:
     def __init__(self, geometry: Geometry, attributes: dict):
         self.geometry = geometry
         self.attributes = attributes
+
+    @property
+    def __geo_interface__(self) -> dict:
+        geo = {
+            "type": "Feature",
+            "geometry": self.geometry.__geo_interface__,
+            "properties": self.attributes
+        }
+        return geo
 
     @classmethod
     def from_geojson(cls, geojson: dict, crs: CRSType = None):
@@ -18,3 +30,59 @@ class Feature:
     @property
     def to_geojson(self) -> dict:
         return feature_to_geojson(self.geometry.to_wkt, self.attributes)
+
+
+class FeatureCollection:
+    def __init__(self, features: list[Feature]):
+        self.features = features
+
+    def to_geojson(self, path: str | None = None, crs: CRSType = None)-> dict:
+        if crs is None:
+            # construct dict of each crs used by features and count how often they occur
+            found_crs = {}
+            for i in self.features:
+                if i.geometry.crs not in found_crs:
+                    found_crs[i.geometry.crs] = 0
+                found_crs[i.geometry.crs] += 1
+
+            if len(found_crs) > 1:
+                # FeatureCollection contains multiple CRS, transform to the most common
+                target_crs = ""
+                crs_count = 0
+                for crs, count in found_crs:
+                    if count > crs_count:
+                        target_crs = crs
+            else:
+                target_crs = list(found_crs.keys())[0]
+        else:
+            target_crs = crs
+
+        # transform feature to uniform target_crs
+        for feat in self.features:
+            if feat.geometry.crs != target_crs:
+                # transform it to mode_crs and replace geometry of feature
+                feat.geometry = feat.geometry.transform(target_crs)
+
+        geojson =  {
+            "type": "FeatureCollection",
+            "features": [i.to_geojson for i in self.features],
+            "crs": {
+                "type": "name",
+                "properties": {
+                    "name": target_crs
+                }
+            }
+        }
+
+        if path:
+            # write to file
+            # TODO: check file path exists
+            with open(path, "w") as f:
+                json.dump(geojson, f, indent=4)
+        return geojson
+
+    # @classmethod
+    # def from_geojson(cls, object: str | dict):
+    #     if isinstance(object, str):
+    #         # if path, read from file
+
